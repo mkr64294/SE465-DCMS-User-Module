4c8e46e2568a258a73a6298cfd09f81fe42fb472
mkr64294 <mark-rice@bethel.edu>
2019-11-25 12:19:32 -0600

    Tweaked some stuff

---
+++ createaccount.js
@@ -0,0 +1,44 @@
+$(function() {
+  $('.btn-6')
+    .on('mouseenter', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+			$(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+    	$(this).find('span').css({top:relY, left:relX})
+    });
+  $('[href=#]').click(function(){return false});
+});
+
+import { Auth } from 'aws-amplify';
+
+Auth.signUp({
+    username,
+    password,
+    attributes: {
+        email,          // optional
+        phone_number,   // optional - E.164 number convention
+        // other custom attributes 
+    },
+    validationData: []  //optional
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+// After retrieving the confirmation code from the user
+Auth.confirmSignUp(username, code, {
+    // Optional. Force user confirmation irrespective of existing alias. By default set to True.
+    forceAliasCreation: true    
+}).then(data => console.log(data))
+  .catch(err => console.log(err));
+
+Auth.resendSignUp(username).then(() => {
+    console.log('code resent successfully');
+}).catch(e => {
+    console.log(e);
+});
